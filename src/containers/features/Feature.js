d846aa493b62b6d00f92a60864746966950fe289
ranjeet <ranjeetgautam13032@gmail.com>
2022-01-23 13:01:22 +0530

    component structure

--- src/containers/features/Feature.js
+++ src/containers/features/Feature.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./feature.css";
-
 const Feature = () => {
   return <div></div>;
 };
---
+++ src/containers/whatGPT3/whatGPT3.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./whatGPT3.css";
+
+const whatGPT3 = () => {
+  return <div></div>;
+};
+
+export default whatGPT3;
